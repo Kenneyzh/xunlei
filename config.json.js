f0174abf8e365f02c906626e39a14876f2057274
Kenney <zql254@vip.qq.com>
2017-05-06 12:43:33 +0800

    fix config

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
-    "build_date": "2017-05-06_12:15:18", 
+    "build_date": "2017-05-06 12:42:55", 
     "description": "远程下载", 
     "home_url": "Module_xunlei.asp", 
-    "md5": "f3209a7b126a273750330d1f2d687464", 
+    "md5": "0c9cd274d20b09ed57fdcee843f29126", 
     "module": "xunlei", 
     "name": "xunlei", 
     "tar_url": "xunlei/xunlei.tar.gz", 
