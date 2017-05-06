5cc2bbe160db85af4bf4b6175f1ab9d8898a7262
Kenney <zql254@vip.qq.com>
2017-05-06 12:16:10 +0800

    add webui link

--- config.json.js
+++ config.json.js
@@ -1,8 +1,11 @@
 {
+    "build_date": "2017-05-06_12:15:18", 
     "description": "远程下载", 
     "home_url": "Module_xunlei.asp", 
-    "md5": "94b730e25bfc07b9087cb10658c70ae8", 
+    "md5": "f3209a7b126a273750330d1f2d687464", 
     "module": "xunlei", 
+    "name": "xunlei", 
+    "tar_url": "xunlei/xunlei.tar.gz", 
     "title": "xunlei", 
-    "version": "0.0.2"
+    "version": "0.0.3"
 }
