0db8149c8853abc74ebab405eca36178ba5d7fdd
Kenney <zql254@vip.qq.com>
2017-05-06 10:44:45 +0800

    add into softcenter

---
+++ config.json.js
@@ -0,0 +1,8 @@
+{
+    "description": "远程下载", 
+    "home_url": "Module_xunlei.asp", 
+    "md5": "fd5a66d3c085237d923d0440f1c3619c", 
+    "module": "xunlei", 
+    "title": "xunlei", 
+    "version": "0.0.1"
+}
